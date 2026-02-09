e7f302c5e351d7191f1fda7b9b034384487a930d
Alexandre Bleuler <alexandre.bleuler@yahoo.com>
2026-02-09 16:42:27 +0100

    modif test split

--- test_split.py
+++ test_split.py
@@ -5,7 +5,13 @@ nbp     = globCom.size
 rank    = globCom.rank
 name    = MPI.Get_processor_name()
 
-com = globCom.Split(color=(rank!=0), key=rank)
+if rank != 0:
+    com_calculs = globCom.Split(color=(rank!=0), key=rank)
+else:
+    com_affichage = globCom.Split(color=(rank!=0), key=rank)
+if rank != 0:
+    com.Set_name("Groupe de calcul")
+else:
+    com.Set_name("Groupe d'affichage")
 print(com.size)
-print(f"processus {rank} : {com.rank}")
-
+print(f"communicator {com.Get_name()} processus {rank} : {com.rank}")
